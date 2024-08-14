4b04e15604c423869cee846d577b059cd4557f67
lazbord <lazarebordereaux@yahoo.fr>
2024-08-14 11:11:14 +0200

    beginning of the main app

--- App.tsx
+++ App.tsx
@@ -1,8 +1,40 @@
 import * as React from 'react';
-import Login from './Login';
+import { StyleSheet, View } from 'react-native';
 
 export default function App() {
   return (
-    <Login></Login>
+    <View style={styles.background}>
+      <View style={styles.main}>
+      </View>
+      <View style={styles.footer}>
+      </View>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    backgroundColor: 'black',
+  },
+  main: {
+    flex: 1,
+    top: 10,
+    width: '55%',
+    height: 'auto',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'white',
+    borderRadius: 10,
+    marginBottom: 10, // This margin will now be respected
+  },
+  footer: {
+    width: '100%',
+    height: 90,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'green',
+  },
+});
